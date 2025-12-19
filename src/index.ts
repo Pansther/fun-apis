eb117ca70d3c638ec1efd022761c67c3e2d4d1c2
Pansther <werawit_boat@windowslive.com>
2025-12-19 10:52:27 +0700

    buid: Remove build config

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,8 @@ import { Elysia } from "elysia";
 
 import { IsFridayModule } from "./modules/is_friday";
 
-const app = new Elysia().use(IsFridayModule);
-//.listen(3000);
+const app = new Elysia().use(IsFridayModule).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
-
-export default app;
