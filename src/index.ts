1a73c2b1b20b905f896d204084915e8892066be6
Pansther <werawit_boat@windowslive.com>
2025-12-19 09:40:23 +0700

    build: Update config

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import { Elysia } from "elysia";
 
 import { IsFridayModule } from "./modules/is_friday";
 
-const app = new Elysia().use(IsFridayModule).listen(3000);
+const app = new Elysia().use(IsFridayModule);
+//.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
+
+export default app;
